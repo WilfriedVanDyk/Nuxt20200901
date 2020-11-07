4964127dffa7d165f510b7f93e4a5b565fbee075
WilfriedVanDyk <wilfriedvandyk@gmail.com>
2020-11-07 16:33:16 +0100

    widget poging 1

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,29 @@
 <template>
   <div class="container">
     <h1>Evenementen pagina</h1>
+    <!-- <script type="text/javascript" src="https://projectaanvraag-api.uitdatabank.be/widgets/layout/fae611103435362fb39420f91fc8b4e1.js"></script> -->
+    <!-- <client-only> <script type="application/javascript" src="https://projectaanvraag-api.uitdatabank.be/widgets/layout/fae611103435362fb39420f91fc8b4e1.js"></script> </client-only> -->
+    <div id="widget"></div>
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
+/* eslint-disable no-useless-escape */
 // import Dashboard from '~/components/Dashboard'
 // import UitEvenement from '~/components/UitEvenement'
+// import postscribe from 'postscribe'
 export default {
-  layout: 'home',
   components: { },
   data () {
     return { }
   },
+  // layout: 'home',
+  // mounted () {
+  //   if (process.browser) {
+  //     postscribe('#widget', '<script type="application/javascript" src="https://projectaanvraag-api.uitdatabank.be/widgets/layout/fae611103435362fb39420f91fc8b4e1.js"><\/script>')
+  //   }
+  // },
   methods: { }
   // head () {
   //   return {
