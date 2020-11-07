460a322bd11d0d598ca9f000118159fadf5bdfdd
brampauwelyn <brampauw@gmail.com>
2020-11-07 21:20:21 +0100

    Added widget page

---
+++ pages/Widget.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Evenementen pagina</h1>
+    <div id="widget"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Widget page',
+    script: [
+      { hid: 'widgets', src: 'https://projectaanvraag-api.uitdatabank.be/widgets/layout/fae611103435362fb39420f91fc8b4e1.js' }
+    ]
+  }
+}
+</script>
