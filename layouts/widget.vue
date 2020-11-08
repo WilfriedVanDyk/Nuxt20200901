4ca8a31b43acce937dc1f04d2433c1efe284a119
brampauwelyn <brampauw@gmail.com>
2020-11-08 20:20:59 +0100

    Use seperate layout for widget page

---
+++ layouts/widget.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Widget</h1>
+    <div class="cultuurnet-widgets"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Widget page',
+    script: [
+      { hid: 'widgets', src: 'https://projectaanvraag-api.uitdatabank.be/widgets/layout/fae611103435362fb39420f91fc8b4e1.js', body: true }
+    ]
+  }
+}
+</script>
